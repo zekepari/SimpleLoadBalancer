219df27939a8a66d46f170944527a07afab7a02c
Zeke Parry <pariezekiel@gmail.com>
2022-04-18 00:43:36 +1000

    update registered servers list over hashmap.

--- src/main/java/me/zekepari/simpleloadbalancer/SimpleLoadBalancer.java
+++ src/main/java/me/zekepari/simpleloadbalancer/SimpleLoadBalancer.java
@@ -1,16 +1,19 @@
 package me.zekepari.simpleloadbalancer;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.player.KickedFromServerEvent;
 import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
-import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import org.slf4j.Logger;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 @Plugin(
         id = "simpleloadbalancer",
@@ -24,7 +27,7 @@ public class SimpleLoadBalancer {
     private final ProxyServer proxyServer;
     private final Logger logger;
 
-    private final List<RegisteredServer> servers = new ArrayList<>();
+    private final List<RegisteredServer> tryServers = new ArrayList<>();
 
     @Inject
     private SimpleLoadBalancer(ProxyServer server, Logger logger) {
@@ -34,26 +37,30 @@ public class SimpleLoadBalancer {
 
     @Subscribe
     private void onProxyInitialization(ProxyInitializeEvent event) {
-        for (String serverName : proxyServer.getConfiguration().getAttemptConnectionOrder()) {
-            proxyServer.getServer(serverName).ifPresent(servers::add);
-        }
-    }
-
-    @Subscribe
-    private void onKickedFromServer(KickedFromServerEvent event) {
-        if (event.kickedDuringServerConnect()) {
-            return;
-        }
-
-        getLowestServer().ifPresent(registeredServer -> event.setResult(KickedFromServerEvent.RedirectPlayer.create(registeredServer)));
+        tryServers.addAll(proxyServer.getAllServers());
+        updateAllServers();
     }
 
     @Subscribe
     private void onServerPreConnection(ServerPreConnectEvent event) {
-        event.setResult(ServerPreConnectEvent.ServerResult.allowed(getLowestServer().get()));
+        Optional<RegisteredServer> optionalServer = proxyServer.getAllServers().stream().min(Comparator.comparingInt(server -> server.getPlayersConnected().size()));
+        optionalServer.ifPresent(server -> event.setResult(ServerPreConnectEvent.ServerResult.allowed(server)));
+        updateAllServers();
     }
 
-    private Optional<RegisteredServer> getLowestServer() {
-        return proxyServer.getAllServers().stream().filter(servers::contains).min(Comparator.comparingInt(server -> server.getPlayersConnected().size()));
+    @Subscribe
+    private void onKickFromServer(KickedFromServerEvent event) {
+        Optional<RegisteredServer> optionalServer = proxyServer.getAllServers().stream().filter(server -> server != event.getServer()).min(Comparator.comparingInt(server -> server.getPlayersConnected().size()));
+        optionalServer.ifPresent(server -> event.setResult(KickedFromServerEvent.RedirectPlayer.create(server)));
+        updateAllServers();
+    }
+
+    private void updateAllServers() {
+        for (RegisteredServer server : tryServers) {
+            server.ping().thenAccept(serverPing -> proxyServer.registerServer(server.getServerInfo())).exceptionally(result -> {
+                proxyServer.unregisterServer(server.getServerInfo());
+                return null;
+            });
+        }
     }
 }
