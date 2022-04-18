05e0724bccd71344e5ccb47957d6dbcc20014c28
Zeke Parry <pariezekiel@gmail.com>
2022-04-18 14:22:52 +1000

    xymb balance calculator

--- src/main/java/me/zekepari/simpleloadbalancer/SimpleLoadBalancer.java
+++ src/main/java/me/zekepari/simpleloadbalancer/SimpleLoadBalancer.java
@@ -2,13 +2,13 @@ package me.zekepari.simpleloadbalancer;
 
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.player.KickedFromServerEvent;
 import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
-import org.slf4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -25,27 +25,43 @@ import java.util.Optional;
 public class SimpleLoadBalancer {
 
     private final ProxyServer proxyServer;
-    private final Logger logger;
 
     private final List<RegisteredServer> servers = new ArrayList<>();
+    static int counter = 0;
 
     @Inject
-    private SimpleLoadBalancer(ProxyServer server, Logger logger) {
+    private SimpleLoadBalancer(ProxyServer server) {
         this.proxyServer = server;
-        this.logger = logger;
     }
 
     @Subscribe
     private void onProxyInitialization(ProxyInitializeEvent event) {
-        servers.addAll(proxyServer.getAllServers());
         updateAllServers();
     }
 
     @Subscribe
     private void onServerPreConnection(ServerPreConnectEvent event) {
-        Optional<RegisteredServer> optionalServer = servers.stream().min(Comparator.comparingInt(server -> server.getPlayersConnected().size()));
-        optionalServer.ifPresent(server -> event.setResult(ServerPreConnectEvent.ServerResult.allowed(server)));
-        updateAllServers();
+        int highestCount = 0;
+        int serverNumber = 0;
+
+        for (RegisteredServer server : servers) {
+            if (server.getPlayersConnected().size() > highestCount) {
+                highestCount = server.getPlayersConnected().size();
+            }
+            serverNumber++;
+        }
+
+        int serverChoice = counter % servers.size();
+
+        if (serverChoice == serverNumber) {
+            serverChoice++;
+            serverChoice %= servers.size();
+        }
+
+        counter++;
+
+        RegisteredServer selectedServer = servers.get(serverChoice);
+        event.setResult(ServerPreConnectEvent.ServerResult.allowed(selectedServer));
     }
 
     @Subscribe
@@ -58,11 +74,14 @@ public class SimpleLoadBalancer {
         updateAllServers();
     }
 
+    @Subscribe
+    private void onDisconnection(DisconnectEvent event) {
+        updateAllServers();
+    }
+
     private void updateAllServers() {
         for (RegisteredServer server : proxyServer.getAllServers()) {
-            server.ping().thenAccept(serverPing -> {
-                servers.add(server);
-            }).exceptionally(result -> {
+            server.ping().thenAccept(serverPing -> servers.add(server)).exceptionally(result -> {
                 servers.remove(server);
                 return null;
             });
