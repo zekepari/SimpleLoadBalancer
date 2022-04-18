723232d1fa1429ba931940b1608b0fb16dda835b
Zeke Parry <pariezekiel@gmail.com>
2022-04-18 15:44:08 +1000

    timed smallest calculator

--- src/main/java/me/zekepari/simpleloadbalancer/SimpleLoadBalancer.java
+++ src/main/java/me/zekepari/simpleloadbalancer/SimpleLoadBalancer.java
@@ -3,7 +3,6 @@ package me.zekepari.simpleloadbalancer;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.player.KickedFromServerEvent;
-import com.velocitypowered.api.event.player.ServerConnectedEvent;
 import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
@@ -11,8 +10,9 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import org.slf4j.Logger;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @Plugin(
@@ -27,7 +27,8 @@ public class SimpleLoadBalancer {
     private final ProxyServer proxyServer;
     private final Logger logger;
 
-    private final HashMap<RegisteredServer, Integer> servers = new HashMap<>();
+    private final ArrayList<RegisteredServer> servers = new ArrayList<>();
+    private static RegisteredServer smallestServer;
 
     @Inject
     private SimpleLoadBalancer(ProxyServer server, Logger logger) {
@@ -37,50 +38,44 @@ public class SimpleLoadBalancer {
 
     @Subscribe
     private void onProxyInitialization(ProxyInitializeEvent event) {
-        updateAllServers();
+        pingAllServers();
+        updateSmallestServer();
     }
 
     @Subscribe
     private void onServerPreConnection(ServerPreConnectEvent event) {
-        event.setResult(ServerPreConnectEvent.ServerResult.allowed(getLowestServer(null)));
-    }
-
-    @Subscribe
-    private void onServerConnection(ServerConnectedEvent event) {
-        servers.replace(event.getServer(), servers.get(event.getServer()) + 1);
+        event.setResult(ServerPreConnectEvent.ServerResult.allowed(smallestServer));
     }
 
     @Subscribe
     private void onKickFromServer(KickedFromServerEvent event) {
-        servers.replace(event.getServer(), servers.get(event.getServer()) - 1);
         if (event.kickedDuringServerConnect()) {
             return;
         }
-        event.setResult(KickedFromServerEvent.RedirectPlayer.create(getLowestServer(event.getServer())));
+        event.setResult(KickedFromServerEvent.RedirectPlayer.create(smallestServer));
     }
 
-    private RegisteredServer getLowestServer(RegisteredServer server) {
-        Map.Entry<RegisteredServer, Integer> min = null;
-        for (Map.Entry<RegisteredServer, Integer> entry : servers.entrySet()) {
-            if (min == null || min.getValue() > entry.getValue() && entry.getKey() != server) {
-                min = entry;
-            }
-        }
-        assert min != null;
-        return min.getKey();
+    private void updateSmallestServer() {
+        proxyServer.getScheduler()
+                .buildTask(this, () -> {
+                    Optional<RegisteredServer> minServer = servers.stream().min(Comparator.comparingInt(server -> server.getPlayersConnected().size()));
+                    minServer.ifPresent(server -> smallestServer = server);
+                })
+                .repeat(400L, TimeUnit.MILLISECONDS)
+                .schedule();
     }
 
-    private void updateAllServers() {
+    private void pingAllServers() {
         proxyServer.getScheduler()
                 .buildTask(this, () -> {
                     for (RegisteredServer server : proxyServer.getAllServers()) {
                         server.ping().thenAccept(serverPing -> {
-                            if (!servers.containsKey(server)) {
-                                servers.put(server, server.getPlayersConnected().size());
+                            if (!servers.contains(server)) {
+                                servers.add(server);
                                 logger.info("Added " + server.getServerInfo().getName() + " to servers list.");
                             }
                         }).exceptionally(result -> {
-                            if (servers.containsKey(server)) {
+                            if (servers.contains(server)) {
                                 servers.remove(server);
                                 logger.info("Removed " + server.getServerInfo().getName() + " from servers list.");
                             }
@@ -88,7 +83,7 @@ public class SimpleLoadBalancer {
                         });
                     }
                 })
-                .repeat(5L, TimeUnit.SECONDS)
+                .repeat(2L, TimeUnit.SECONDS)
                 .schedule();
     }
 }
